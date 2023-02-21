fd34515e736d1576fe4f222ca28bc4ce9a140ffe
NdaaaXyz <ndaatzy123@gmail.com>
2023-02-21 13:21:52 +0700

    yes

---
+++ settings.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+global.creator = 'Ndaaa' 
+global.apikey = ["Ndaa", "Ndaakey", "Key"]
+global.aikey = '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'
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update'${__filename}'`)
+	delete require.cache[file]
+	require(file)
+})
